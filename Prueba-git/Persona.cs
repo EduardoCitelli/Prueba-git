a706c23f00482fc5568e963b1a4234b70520c827
Eduardo Citelli <eduardo.citelli@redjar.com.ar>
2023-07-08 11:25:42 -0300

    creada clase Persona

---
+++ Prueba-git/Persona.cs
@@ -0,0 +1,14 @@
+﻿namespace Prueba_git
+{
+    public class Persona
+    {
+        public string Nombre { get; set; }
+
+        public string Apellido { get; set; }
+
+        public string Saludar()
+        {
+            return $"hola mi nombre es {Nombre} y mi apellido es {Apellido}";
+        }
+    }
+}
